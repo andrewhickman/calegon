2f4efbd3309a3ea43c627e4140c40a29e602f182
Andrew Hickman <andrew.hickman1@sky.com>
2018-12-02 18:45:47 +0000

    rewrite type-system

--- type-system/src/automaton/dfa/mod.rs
+++
@@ -1,9 +0,0 @@
-mod reduce;
-mod scheme;
-mod subsume;
-#[cfg(test)]
-mod tests;
-mod ty;
-
-pub use self::scheme::Scheme;
-pub use self::ty::*;
--- type-system/src/automaton/nfa/mod.rs
+++
@@ -1,8 +0,0 @@
-mod biunify;
-mod build;
-mod scheme;
-mod ty;
-
-pub use self::scheme::*;
-
-pub use self::ty::*;
